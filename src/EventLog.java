5500d651fbb42fdd39af3722337b4e95c2342080
Marshall Munoz <marshmun@gmail.com>
2017-07-27 12:30:03 -0500

    adding in methods and additonal class

--- src/EventLog.java
+++ src/EventLog.java
@@ -1,2 +1,21 @@
+import java.awt.*;
+
 public class EventLog {
+    private List<Event> eventList;
+
+    public boolean addEvent(Event event) {
+        return false;
+    }
+
+    public int getNumEvents() {
+        return 0;
+    }
+
+    public EventLog() {
+
+    }
+
+
 }
+//    public int getNumEvents () //which returns the size of the list
+//    public EventLog () which initializes the list
---
+++ src/JavaBean.java
@@ -0,0 +1,4 @@
+public class JavaBean {
+    String name;
+    String action;
+}
